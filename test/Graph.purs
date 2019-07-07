385e076fc815aa56036f8775a36135bf98ff9d6a
F_ <hn.fspace@outlook.com>
2019-07-07 20:43:07 +0900

    test: 壊れたテストの修正とルートに関するテストの追加

--- test/Graph.purs
+++ test/Graph.purs
@@ -3,12 +3,13 @@ module Test.Graph (spec) where
 import Prelude
 
 import App.Model (Skill(..), SkillCategoryGap, SkillTable, categories, indices)
-import App.Model.Graph (SkillGraph, build, route, cost)
+import App.Model.Graph (SkillGraph, build, cost, paths, routes)
 import App.Model.Graph.Option (Option(..), toTransform)
 import App.Model.Table (update)
 import Control.Monad.Error.Class (class MonadThrow)
+import Data.Array (sort)
 import Data.Enum (toEnum)
-import Data.Maybe (fromJust, isJust, isNothing)
+import Data.Maybe (Maybe, fromJust, isJust, isNothing)
 import Data.Set (Set, empty, fromFoldable, singleton)
 import Data.Traversable (for_)
 import Effect.Exception (Error)
@@ -22,88 +23,127 @@ spec = describe "Graph" do
     let result = build (pure false) $ mempty
     result `shouldSatisfy` isNothing
   it "full-graph" do
-    let result = build (pure true) $ mempty
-    result `shouldSatisfy` isJust
-
-    let graph = unsafePartial fromJust result
+    graph <- fromJustGraph $ build (pure true) $ mempty
     for_ (Skill <$> categories <*> indices) \skill -> do
-      cost graph skill `shouldEqual` 0
+      cost skill graph `shouldEqual` 0
   it "cost-without-options" do
-    let graph = testGraph empty
-    cost graph (skill_ 0 0) `shouldEqual` 4
-    cost graph (skill_ 0 8) `shouldEqual` 3
-    cost graph (skill_ 1 3) `shouldEqual` 3
-    cost graph (skill_ 2 2) `shouldEqual` 2
-    cost graph (skill_ 2 9) `shouldEqual` 0
-    cost graph (skill_ 3 1) `shouldEqual` 3
-    cost graph (skill_ 4 8) `shouldEqual` 2
-    cost graph (skill_ 5 0) `shouldEqual` 6
-    cost graph (skill_ 5 4) `shouldEqual` 4
+    graph <- fromJustGraph $ testGraph empty
+    cost (skill_ 0 0) graph `shouldEqual` 4
+    cost (skill_ 0 8) graph `shouldEqual` 3
+    cost (skill_ 1 3) graph `shouldEqual` 3
+    cost (skill_ 2 2) graph `shouldEqual` 2
+    cost (skill_ 2 9) graph `shouldEqual` 0
+    cost (skill_ 3 1) graph `shouldEqual` 3
+    cost (skill_ 4 8) graph `shouldEqual` 2
+    cost (skill_ 5 0) graph `shouldEqual` 6
+    cost (skill_ 5 4) graph `shouldEqual` 4
   it "cost-with-fillgap" do
-    let graph = testGraph $ fromFoldable [fillgap1, fillgap2]
-    cost graph (skill_ 0 0) `shouldEqual` 3
-    cost graph (skill_ 0 8) `shouldEqual` 3
-    cost graph (skill_ 1 3) `shouldEqual` 2
-    cost graph (skill_ 2 2) `shouldEqual` 2
-    cost graph (skill_ 2 9) `shouldEqual` 0
-    cost graph (skill_ 3 1) `shouldEqual` 2
-    cost graph (skill_ 4 8) `shouldEqual` 2
-    cost graph (skill_ 5 0) `shouldEqual` 5
-    cost graph (skill_ 5 4) `shouldEqual` 4
+    graph <- fromJustGraph $ testGraph $ fromFoldable [fillgap1, fillgap2]
+    cost (skill_ 0 0) graph `shouldEqual` 3
+    cost (skill_ 0 8) graph `shouldEqual` 3
+    cost (skill_ 1 3) graph `shouldEqual` 2
+    cost (skill_ 2 2) graph `shouldEqual` 2
+    cost (skill_ 2 9) graph `shouldEqual` 0
+    cost (skill_ 3 1) graph `shouldEqual` 2
+    cost (skill_ 4 8) graph `shouldEqual` 2
+    cost (skill_ 5 0) graph `shouldEqual` 5
+    cost (skill_ 5 4) graph `shouldEqual` 4
   it "cost-with-wrapcat" do
-    let graph = testGraph $ singleton wrapcat
-    cost graph (skill_ 0 0) `shouldEqual` 4
-    cost graph (skill_ 0 8) `shouldEqual` 2
-    cost graph (skill_ 1 3) `shouldEqual` 3
-    cost graph (skill_ 2 2) `shouldEqual` 2
-    cost graph (skill_ 2 9) `shouldEqual` 0
-    cost graph (skill_ 3 1) `shouldEqual` 3
-    cost graph (skill_ 4 8) `shouldEqual` 2
-    cost graph (skill_ 5 0) `shouldEqual` 6
-    cost graph (skill_ 5 4) `shouldEqual` 3
+    graph <- fromJustGraph $ testGraph $ singleton wrapcat
+    cost (skill_ 0 0) graph `shouldEqual` 4
+    cost (skill_ 0 8) graph `shouldEqual` 2
+    cost (skill_ 1 3) graph `shouldEqual` 3
+    cost (skill_ 2 2) graph `shouldEqual` 2
+    cost (skill_ 2 9) graph `shouldEqual` 0
+    cost (skill_ 3 1) graph `shouldEqual` 3
+    cost (skill_ 4 8) graph `shouldEqual` 2
+    cost (skill_ 5 0) graph `shouldEqual` 6
+    cost (skill_ 5 4) graph `shouldEqual` 3
   it "cost-with-wrapidx" do
-    let graph = testGraph $ singleton wrapidx
-    cost graph (skill_ 0 0) `shouldEqual` 4
-    cost graph (skill_ 0 8) `shouldEqual` 3
-    cost graph (skill_ 1 3) `shouldEqual` 3
-    cost graph (skill_ 2 2) `shouldEqual` 2
-    cost graph (skill_ 2 9) `shouldEqual` 0
-    cost graph (skill_ 3 1) `shouldEqual` 3
-    cost graph (skill_ 4 8) `shouldEqual` 2
-    cost graph (skill_ 5 0) `shouldEqual` 3
-    cost graph (skill_ 5 4) `shouldEqual` 4
+    graph <- fromJustGraph $ testGraph $ singleton wrapidx
+    cost (skill_ 0 0) graph `shouldEqual` 4
+    cost (skill_ 0 8) graph `shouldEqual` 3
+    cost (skill_ 1 3) graph `shouldEqual` 3
+    cost (skill_ 2 2) graph `shouldEqual` 2
+    cost (skill_ 2 9) graph `shouldEqual` 0
+    cost (skill_ 3 1) graph `shouldEqual` 3
+    cost (skill_ 4 8) graph `shouldEqual` 2
+    cost (skill_ 5 0) graph `shouldEqual` 3
+    cost (skill_ 5 4) graph `shouldEqual` 4
   it "cost-with-diag" do
-    let graph = testGraph $ singleton diag
-    cost graph (skill_ 0 0) `shouldEqual` 4
-    cost graph (skill_ 0 8) `shouldEqual` 3
-    cost graph (skill_ 1 3) `shouldEqual` 3
-    cost graph (skill_ 2 2) `shouldEqual` 2
-    cost graph (skill_ 2 9) `shouldEqual` 0
-    cost graph (skill_ 3 1) `shouldEqual` 2
-    cost graph (skill_ 4 8) `shouldEqual` 2
-    cost graph (skill_ 5 0) `shouldEqual` 6
-    cost graph (skill_ 5 4) `shouldEqual` 4
+    graph <- fromJustGraph $ testGraph $ singleton diag
+    cost (skill_ 0 0) graph `shouldEqual` 4
+    cost (skill_ 0 8) graph `shouldEqual` 3
+    cost (skill_ 1 3) graph `shouldEqual` 3
+    cost (skill_ 2 2) graph `shouldEqual` 2
+    cost (skill_ 2 9) graph `shouldEqual` 0
+    cost (skill_ 3 1) graph `shouldEqual` 2
+    cost (skill_ 4 8) graph `shouldEqual` 2
+    cost (skill_ 5 0) graph `shouldEqual` 6
+    cost (skill_ 5 4) graph `shouldEqual` 4
   it "cost-with-all" do
-    let graph = testGraph $ fromFoldable [fillgap1, fillgap2, wrapcat, wrapidx, diag]
-    cost graph (skill_ 0 0) `shouldEqual` 3
-    cost graph (skill_ 0 8) `shouldEqual` 2
-    cost graph (skill_ 1 3) `shouldEqual` 2
-    cost graph (skill_ 2 2) `shouldEqual` 2
-    cost graph (skill_ 2 9) `shouldEqual` 0
-    cost graph (skill_ 3 1) `shouldEqual` 1
-    cost graph (skill_ 4 8) `shouldEqual` 2
-    cost graph (skill_ 5 0) `shouldEqual` 3
-    cost graph (skill_ 5 4) `shouldEqual` 3
-  it "route" do
-    let graph = testGraph $ fromFoldable [fillgap1, fillgap2, wrapcat, wrapidx, diag]
-    route graph (skill_ 0 10) `shouldEqual`
-      [ skill_ 2 0
-      , skill_ 1 10
-      , skill_ 0 10
+    graph <- fromJustGraph $ testGraph $ fromFoldable [fillgap1, fillgap2, wrapcat, wrapidx, diag]
+    cost (skill_ 0 0) graph `shouldEqual` 3
+    cost (skill_ 0 8) graph `shouldEqual` 2
+    cost (skill_ 1 3) graph `shouldEqual` 2
+    cost (skill_ 2 2) graph `shouldEqual` 2
+    cost (skill_ 2 9) graph `shouldEqual` 0
+    cost (skill_ 3 1) graph `shouldEqual` 1
+    cost (skill_ 4 8) graph `shouldEqual` 2
+    cost (skill_ 5 0) graph `shouldEqual` 3
+    cost (skill_ 5 4) graph `shouldEqual` 3
+  it "unique-path" do
+    graph <- fromJustGraph $ testGraph $ fromFoldable [fillgap1, fillgap2, wrapcat, wrapidx, diag]
+    paths (routes (skill_ 0 10) graph) `shouldEqual`
+      [ [ skill_ 2 0
+        , skill_ 1 10
+        , skill_ 0 10
+        ]
+      ]
+  it "single-path" do
+    graph <- fromJustGraph $ testGraph $ fromFoldable [fillgap1, fillgap2, wrapcat, wrapidx, diag]
+    paths (routes (skill_ 4 3) graph) `shouldEqual`
+      [ [ skill_ 2 4
+        , skill_ 2 3
+        , skill_ 3 3
+        , skill_ 4 3
+        ]
+      ]
+  it "multiple-path" do
+    graph <- fromJustGraph $ testGraph $ fromFoldable [fillgap1, fillgap2, wrapcat, wrapidx, diag]
+    sort (paths (routes (skill_ 2 2) graph)) `shouldEqual`
+      [ [ skill_ 2 0
+        , skill_ 2 1
+        , skill_ 2 2
+        ]
+      , [ skill_ 2 4
+        , skill_ 2 3
+        , skill_ 2 2
+        ]
+      ]
+    sort (paths (routes (skill_ 5 2) graph)) `shouldEqual`
+      [ [ skill_ 0 5
+        , skill_ 0 4
+        , skill_ 0 3
+        , skill_ 0 2
+        , skill_ 5 2
+        ]
+      , [ skill_ 5 8
+        , skill_ 5 9
+        , skill_ 5 10
+        , skill_ 5 0
+        , skill_ 5 1
+        , skill_ 5 2
+        ]
       ]
 
-testGraph :: Set Option -> SkillGraph
-testGraph options = unsafePartial fromJust $ build skills $ toTransform options
+fromJustGraph :: forall m. MonadThrow Error m => Maybe SkillGraph -> m SkillGraph
+fromJustGraph graph = do
+  graph `shouldSatisfy` isJust
+  pure $ unsafePartial fromJust graph
+
+testGraph :: Set Option -> Maybe SkillGraph
+testGraph options = build skills $ toTransform options
   where
     skills :: SkillTable Boolean
     skills = pure false
