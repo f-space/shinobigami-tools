aac6d964717473a7f5475f4cfdc861bbfc363705
F_ <hn.fspace@outlook.com>
2019-07-18 18:11:18 +0900

    style: JSDocによる型指定を一行に

--- src/FFI/CustomEvent.js
+++ src/FFI/CustomEvent.js
@@ -2,18 +2,14 @@
 
 "use strict";
 
-/**
- * @type {<T>(typeArg: string, eventInitDict?: CustomEventInit<T>) => () => CustomEvent<T>}
- */
+/** @type {<T>(typeArg: string, eventInitDict?: CustomEventInit<T>) => () => CustomEvent<T>} */
 exports.customEvent_ = function (typeArg, eventInitDict) {
 	return function () {
 		return new CustomEvent(typeArg, eventInitDict);
 	}
 }
 
-/**
- * @type {<T>(event: CustomEvent<T>) => T}
- */
+/** @type {<T>(event: CustomEvent<T>) => T} */
 exports.detail = function (event) {
 	return event.detail;
 }
--- src/FFI/Document.js
+++ src/FFI/Document.js
@@ -2,9 +2,7 @@
 
 "use strict";
 
-/**
- * @type {(x: number, y: number, document: Document) => () => Element}
- */
+/** @type {(x: number, y: number, document: Document) => () => Element} */
 exports.elementFromPoint_ = function (x, y, document) {
 	return function () {
 		return Document.prototype.elementFromPoint.call(document, x, y);
