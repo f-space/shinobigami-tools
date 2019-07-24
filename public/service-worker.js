6a32fe3c1e127f69f9582913768a25e38e484525
F_ <hn.fspace@outlook.com>
2019-07-25 00:35:09 +0900

    chore: v0.8.2

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "v0.8.1";
+const CACHE_NAME = "v0.8.2";
 
 const FILE_URLS = [
 	".",
--- src/App/Home.purs
+++ src/App/Home.purs
@@ -65,7 +65,7 @@ render _ =
     , HH.footer
       [ HP.class_ $ H.ClassName "information" ]
       [ HH.span_ [ HH.text "Version" ]
-      , HH.span_ [ HH.text "0.8.1" ]
+      , HH.span_ [ HH.text "0.8.2" ]
       , HH.span_ [ HH.text "Author" ]
       , HH.span_ 
         [ HH.text "F_"
